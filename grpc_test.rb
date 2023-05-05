d29c2fb80d5af94ab995a4240a8600c0b5fb59d5
Vitalii Yulieff <wwjurew@gmail.com>
2023-05-05 16:51:13 +0400

    WIP

--- grpc_test.rb
+++ grpc_test.rb
@@ -1,4 +1,4 @@
 # require "grpc"
-require "rspec"
-require "tempfile"
-RSpec::Core::Runner::run(['spec'], Tempfile.new, Tempfile.new)
+# require "rspec"
+# require "tempfile"
+# RSpec::Core::Runner::run(['spec'], Tempfile.new, Tempfile.new)
